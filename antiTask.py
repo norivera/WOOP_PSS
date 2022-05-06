00540e6bbe4595b6078a60c2294f1404af05344c
Alexa Tang <46989357+AlexaTTang@users.noreply.github.com>
2022-05-06 11:05:03 -0700

    adding method outline for schedule class

---
+++ antiTask.py
@@ -0,0 +1,3 @@
+import schedule
+class Task():
+     pass
---
+++ schedule.py
@@ -0,0 +1,18 @@
+class Schedule:
+    taskList #List of task object
+    def __init__(self):
+        pass
+    def createTask():
+        pass
+    def viewTask():
+        pass
+    def deleteTask():
+        pass
+    def writeScheduleToFile():
+        pass
+    def readScheduleToFile():
+        pass
+    def viewSchedule():
+        pass
+    def writeSchedule():
+        pass
