8b1d19db36beb0b23bb63faacc06f61925a4ffd5
Alexa Tang <46989357+AlexaTTang@users.noreply.github.com>
2022-05-06 12:29:51 -0700

    added some doc strings and param lists to methods in schedule and task

--- schedule.py
+++ schedule.py
@@ -2,17 +2,28 @@ class Schedule:
     taskList #List of task object
     def __init__(self):
         pass
-    def createTask():
+    def checkName(name):
+        '''
+        ensure that each task has a unique name for searching purposes
+        '''
         pass
-    def viewTask():
+    def createTask(name, type, sTime, duration, date):
         pass
-    def deleteTask():
+    def viewTask(name):
+        pass
+    def editTask(name, type, sTime, duration, date):
         pass
     def writeScheduleToFile():
         pass
-    def readScheduleToFile():
+    def readScheduleToFile(filename):
         pass
-    def viewSchedule():
+    def viewSchedule(period, startDate):
+        '''
+        printing the schedul efor a specified day, week, or month
+        '''
         pass
-    def writeSchedule():
+    def writeSchedule(fileName, period, startDate):
+        '''
+        updating the schedule for a specified day, week, or month
+        '''
         pass
--- task.py
+++ task.py
@@ -1,10 +1,22 @@
+from xml.dom.expatbuilder import parseString
 from WOOP_PSS.schedule import Schedule
 # Task Class will inherit from schedule
 class Task(Schedule):
     # when a task object is created, pass in values for the attributes
     def __init__(self, name, type, startTime, duration, date):
+        '''
+        initializing the info for each task, and input checking here
+        try catch?
+        '''
         self.name = name
         self.type = type
         self.startTime = startTime
         self.duration = duration
         self.date = date
+    #passing in the taskList from parent or directly accessing in method??
+    def taskOverlap(taskList, task):
+        '''
+        When creating transient tasks, if recurring, check if there is an anti-task to cancel it; if
+        so, the transient task is not overlapping 
+        '''
+        pass
