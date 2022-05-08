65b073ef411c3b5f2db1ba0c83e8e3dd4f26a87e
norivera <89235780+norivera@users.noreply.github.com>
2022-05-08 16:08:53 -0700

    updated menu

--- driver.py
+++ driver.py
@@ -5,6 +5,7 @@ import sys
 #run in command prompt: pip install console-menu
 from consolemenu import *
 from consolemenu.items import *
+# sample code for the menu: https://pypi.org/project/console-menu/ 
 # import recurringTask
 # import transientTask
 
@@ -113,18 +114,26 @@ def main() :
 
 
 
-    mainMenu = ConsoleMenu("Title", "Subtitle")
+    menu = ConsoleMenu("Welcome to PSS")
+    selection_menu = SelectionMenu(["Edit", "Add", "View", "Delete"])
+    submenu_item1 = SubmenuItem("Create schedule", selection_menu, menu)
+    submenu_item2 = SubmenuItem("Import schedule", selection_menu, menu)
+    menu.append_item(submenu_item1)
+    menu.append_item(submenu_item2)
+    menu.show()
 
-        
-    
+    # menu_item2 = MenuItem("Export Schedule")
+
+    #submenu_item = SubmenuItem("Edit Schedule Menu", selection_menu, mainMenu) 
+
 
                                                
-    user.createTask("1", "Class",    10.25,          0.25,      20220506)
-    user.createTask("2", "Class",    8.25,          0.25,      20220510)
-    user.createTask("3", "Class",    9.25,          0.25,      20220601)
+    # user.createTask("1", "Class",    10.25,          0.25,      20220506)
+    # user.createTask("2", "Class",    8.25,          0.25,      20220510)
+    # user.createTask("3", "Class",    9.25,          0.25,      20220601)
 
     #user.viewSchedule("month",20220506) 
-    user.writeScheduleToFile()
+    # user.writeScheduleToFile()
 
 if __name__ == '__main__':
     main()
